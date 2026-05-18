7904d1b387c443ddab7cf47f3cc8e639b487f88a
Santiago Velasquez <santiagoalejandrovelasquezmarc@gmail.com>
2026-05-18 18:44:11 -0400

    feat: Initialize backend project structure with Gradle
    - Added gradle-wrapper.properties for Gradle configuration.
    - Included gradlew and gradlew.bat scripts for Gradle command execution.
    - Created settings.gradle to define the root project name.
    - Implemented main application class ProyectoControlDeRegistroPenitenciarioApplication.
    - Configured application.properties with the application name.
    - Added a test class ProyectoControlDeRegistroPenitenciarioApplicationTests for context loading verification.

---
+++ teams/EquipoBlanco/backend/src/main/java/com/Equipoblanco/proyectoControlDeRegistroPenitenciario/ProyectoControlDeRegistroPenitenciarioApplication.java
@@ -0,0 +1,13 @@
+package com.Equipoblanco.proyectoControlDeRegistroPenitenciario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoControlDeRegistroPenitenciarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoControlDeRegistroPenitenciarioApplication.class, args);
+	}
+
+}
---
+++ teams/EquipoBlanco/backend/src/test/java/com/Equipoblanco/proyectoControlDeRegistroPenitenciario/ProyectoControlDeRegistroPenitenciarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Equipoblanco.proyectoControlDeRegistroPenitenciario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoControlDeRegistroPenitenciarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
