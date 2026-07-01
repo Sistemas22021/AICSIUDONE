b42bcaf80e4c6bb90904d0a7e4ea306fd4418e88
Jorge La Rosa <jluislarosam@gmail.com>
2026-07-01 01:11:37 -0400

    feat(testimony): agregar soporte para cédula y número de caso en el registro de testimonios, y persistir datos en la base de datos

--- teams/naranja/Backend/src/main/java/naranja/custodia_360/models/Testimony.java
+++ teams/naranja/Backend/src/main/java/naranja/custodia_360/models/Testimony.java
@@ -1,7 +1,45 @@
 package naranja.custodia_360.models;
 
-public record Testimony(
-        String content,
-        String sessionId
-) {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Testimony{
+        @Id
+        private String sessionId;
+        
+        private String cedula;
+        private String caseNumber;
+
+        //rutas locales
+        private String audioPath;
+        private String originalTextPath;
+        private String modifiedTextPath;
+
+        public Testimony (){}
+
+        public Testimony (String sessionId, String cedula, String caseNumber, String audioPath, String originalTextPath, String modifiedTextPath){
+                this.sessionId = sessionId;
+                this.cedula = cedula;
+                this.caseNumber = caseNumber;
+                this.audioPath = audioPath;
+                this.originalTextPath = originalTextPath;
+                this.modifiedTextPath = modifiedTextPath;
+        }
+
+        //Getters
+        public String getSessionId() { return sessionId; }
+        public String getCedula() { return cedula; }
+        public String getCaseNumber() { return caseNumber; }
+        public String getAudioPath() { return audioPath; }
+        public String getOriginalTextPath() { return originalTextPath; }
+        public String getModifiedTextPath() { return modifiedTextPath; }
+
+        //setters
+        public void setSessionId(String sessionId) { this.sessionId = sessionId; }
+        public void setCedula(String cedula) { this.cedula = cedula; }
+        public void setCaseNumber(String caseNumber) { this.caseNumber = caseNumber; }
+        public void setAudioPath(String audioPath) { this.audioPath = audioPath; }
+        public void setOriginalTextPath(String originalTextPath) { this.originalTextPath = originalTextPath; }
+        public void setModifiedTextPath(String modifiedTextPath) { this.modifiedTextPath = modifiedTextPath; }
 }
