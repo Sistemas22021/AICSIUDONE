4608266b2b32d77d68221d3ad7085089e2ee1c37
johnmsalazarm <johnmsalazarm@gmail.com>
2026-07-06 15:07:44 -0400

    feat(ui-components): agregar componente TestigosInput al ecosistema

---
+++ teams/main/amarrillo/frontend/src/components/TestigosInput.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+// Nexo Criminal Platform Component
+export const TestigosInput = () => {
+    return <div>TestigosInput Module Loaded Successfully</div>;
+};
+export default TestigosInput;
