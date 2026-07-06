800d35d76c0ceac844ad31366cd6e21ee1b8482d
johnmsalazarm <johnmsalazarm@gmail.com>
2026-07-06 15:07:23 -0400

    feat(ubicacion-ui): crear vista de administracion de Ubicaciones

---
+++ teams/main/amarrillo/frontend/src/pages/Ubicaciones.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+// Nexo Criminal Platform Component
+export const Ubicaciones = () => {
+    return <div>Ubicaciones Module Loaded Successfully</div>;
+};
+export default Ubicaciones;
