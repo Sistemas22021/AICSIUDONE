8f4358bd1bf8c90b688de9b9c990926c92fc89f9
johnmsalazarm <johnmsalazarm@gmail.com>
2026-07-06 15:07:17 -0400

    feat(persona-ui): implementar vista de Personas y componente FormPersona

---
+++ teams/main/amarrillo/frontend/src/components/FormPersona.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+
+// Nexo Criminal Platform Component
+export const FormPersona = () => {
+    return <div>FormPersona Module Loaded Successfully</div>;
+};
+export default FormPersona;
